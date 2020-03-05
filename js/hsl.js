043b79e6917c6643d940dc9136b6b7788e132bfe
Nakki <nakkikone@Nakkikone.local>
2020-03-05 18:35:06 +0200

    HSL

--- js/hsl.js
+++ js/hsl.js
@@ -1,51 +1,91 @@
 'use strict';
 const apiUrl = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
 
-//put graphQL query here
-const graphql = `{
-  stopsByRadius(lat:60.258,lon:24.844,radius:1100){
-    edges {
-      node {
-        stop { 
-          gtfsId 
-          name
-        }
-        distance
+const schedule = [];
+const stopId = [
+  4150202,
+  4150228,
+  4150201,
+  4150296,
+  4150269,
+  4150268,
+  4150261
+];
+
+const getTime = (seconds) => {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor(seconds / 60) - (hours * 60);
+  // add leading zero to minutes string if needed
+  return `${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+};
+
+const getStopData = (id) => {
+  return `{
+  stop(id: "HSL:${id}") {
+    name
+    stoptimesWithoutPatterns {
+      scheduledArrival
+      realtimeArrival
+      arrivalDelay
+      scheduledDeparture
+      realtimeDeparture
+      departureDelay
+      realtime
+      realtimeState
+      serviceDay
+      headsign
+      trip {
+        routeShortName
+        tripHeadsign
       }
     }
   }
 }`
+};
 
-// Present received data
-const showResults = async (result) => {
-  const list = await result.data.stopsByRadius.edges; // list = path to the selected array
-  for(let i in list) {
-    const obj = list[i].node.stop.name;  
-    const node = document.createElement('LI');
-    const textnode = document.createTextNode(obj);
+const showResult = () => {
+  for(let i in schedule){
+    const node = document.createElement('li');
+    const textnode = document.createTextNode(schedule[i]);
     node.appendChild(textnode);
     document.querySelector('.hsl-list').appendChild(node);
   }
 };
+// Present received data
+const makeArray = async (result) => {
+  const stop = result.data.stop;
+  for (let i=0; i<2;i++) {
+    const ride = await stop.stoptimesWithoutPatterns[i];
+    // schedule.push(`time: "${getTime(ride.scheduledDeparture)}", line: "${ride.trip.routeShortName}", 
+    //  destination: "${ride.headsign !== null ? ride.headsign : ride.trip.tripHeadsign}", stop: "${stop.name}"`);
+    // };
+    schedule.push(`${getTime(ride.scheduledDeparture)}, ${ride.trip.routeShortName}, 
+    ${ride.headsign !== null ? ride.headsign : ride.trip.tripHeadsign}, ${stop.name}`);
+   };
+    schedule.sort();
+  };
 
 // Fetch data from HSL
-const fetchData = async () => {
+const fetchData = async (i) => {
   let response;
   response = await fetch(apiUrl, {
-    method: 'POST', // or 'PUT'
+    method: 'POST',
     headers: {
       'Content-Type': 'application/graphql',
     },
-    body: `${graphql}`
+    body: getStopData(stopId[i])
   })
   let data = await response.json();
   return data;
 };
 
 const hslQuery = async () => {
-  const response = await fetchData();
-  const result = await response;
-  showResults(result);
+  for (let i in stopId) {
+    const response = await fetchData(i);
+    const result = await response;
+    makeArray(result);
+  }
+  showResult();
 };
 
 hslQuery();
